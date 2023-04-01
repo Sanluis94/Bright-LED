d1b547e3064fb3dd1f3b3c0c20c2188d7441eaff
Sanluis94 <luisadamski94@gmail.com>
2023-04-01 14:52:20 -0300

    COntagem de FPS e UPS, player e joystick

---
+++ app/src/main/java/com/example/projetointegrador/Game.java
@@ -0,0 +1,108 @@
+package com.example.projetointegrador;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import androidx.core.content.ContextCompat;
+
+class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Joystick joystick;
+    private GameLoop gameLoop ;
+    private Context context;
+    private final Player player;
+
+    public Game(Context context) {
+        super(context);
+
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        joystick = new Joystick(275,700,70,40);
+
+        player = new Player(getContext(),2*500,500,30);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if(joystick.isPressed((double)event.getX(),(double)event.getY())){
+                    joystick.setIsPressed(true);
+                }
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                if(joystick.getIsPressed()) {
+                    joystick.setActuator((double) event.getX(), (double) event.getY());
+                }
+                return true;
+
+            case MotionEvent.ACTION_UP:
+                joystick.setIsPressed(false);
+                joystick.resetActuator();
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        gameLoop.startLoop();
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+
+        joystick.draw(canvas);
+        player.draw(canvas);
+    }
+
+    public void drawUPS(Canvas canvas){
+
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(getContext(),R.color.red);
+        paint.setColor(color);
+        paint.setTextSize(20);
+        canvas.drawText("UPS: "+averageUPS, 100,100,paint);
+
+    }
+
+    public void drawFPS(Canvas canvas){
+
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(getContext(),R.color.red);
+        paint.setColor(color);
+        paint.setTextSize(20);
+        canvas.drawText("FPS: "+averageFPS, 100,200,paint);
+
+    }
+
+    public void update() {
+
+        joystick.update();
+        player.update(joystick);
+    }
+}
---
+++ app/src/main/java/com/example/projetointegrador/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.projetointegrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    Sensor sensor;
+    SensorManager sensorManager;
+
+    TextView sampleText;
+
+    Window window = getWindow();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        setContentView(new Game(this));
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        /*if(event.values[0]>40){
+
+        }
+        else if(event.values[0]<10){
+
+        }*/
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+}
